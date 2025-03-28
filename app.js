c35ad9ce80573c725c533f6ebba856586981ee3c
Alvaro <johan.s310806@gmail.com>
2025-03-28 11:38:25 -0500

    Realizado de get y post para categorias

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import categoriasRouter from "./routes/categoriasRoutes.js";
+
+export const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/categorias", categoriasRouter)
+
+app.listen(3000, () => {
+  console.log("HOLA");
+});
---
+++ models/Categoria.js
@@ -0,0 +1,35 @@
+import connection from "../utils/db.js";
+
+class Categoria{
+  constructor(nombre, descripcion) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+  }
+  /**
+   * Metodo para obtener los registros de la base de datos
+   * @returns {Array} Listado de las categorias de un arreglo.
+   */
+  async getAll() {
+    try {
+      const [rows] = await connection.query("SELECT * FROM categorias");
+      return rows;
+    } catch (error) {
+      throw new Error("Error al obtener las categorías.")
+    }
+  }
+
+  async create() {
+    try {
+      const [result] = await connection.query("INSERT INTO categorias(nombre, descripcion) values (?,?)", [this.nombre, this.descripcion]);
+      return {
+        id: result.id,
+        nombre: this.nombre,
+        descripcion: this.descripcion
+      }
+    } catch (error) {
+      throw new Error("Error al crear la categoria.")
+    }
+  }
+}
+
+export default Categoria;
